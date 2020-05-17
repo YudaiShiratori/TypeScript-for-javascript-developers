c48fdf4cade8948f03be02f78c8e8ed41d55cfd2
Yudai Shiratori <toritorikebabu1023x@gmail.com>
2020-05-17 15:36:35 +0900

    assertion

---
+++ src/as-constant.ts
@@ -0,0 +1,16 @@
+export {}
+
+let name = "John"
+
+name = "Sam"
+
+let nickname = "Ham" as const
+// nickname = "aa"
+
+let profile = {
+    name: "Croud",
+    height: 173
+} as const
+
+// profile.name = "Ham"
+// profile.height = 180
---
+++ src/type-assertion.ts
@@ -0,0 +1,10 @@
+export {};
+
+let name: any = "Han"
+
+// let length: number = name.length;
+let length = name.length as number
+// let length = (name as string).length
+// let length = (<string>name).length
+
+// length = "foo"
